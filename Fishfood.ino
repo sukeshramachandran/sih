9066ae1fe92c87a9bdc2bb3fc4d3915bb6d3b1bd
sukeshramachandran <69029776+sukeshramachandran@users.noreply.github.com>
2020-08-26 14:00:14 +0530

    Create Fishfood.ino

---
+++ Fishfood.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+ 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // a maximum of eight servo objects can be created 
+ 
+int pos = 0;    // variable to store the servo position 
+
+long FISHFEEDER = 43200000; // 12 hours between feeding
+long endtime; 
+long now;
+
+void setup() 
+{ 
+ 
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  
+  myservo.write(0);
+  delay(15);
+  
+}
+ 
+void loop() 
+{ 
+  now = millis();
+  endtime = now + FISHFEEDER;
+  
+  while(now < endtime) {
+   myservo.write(0);
+   delay(20000);
+   now = millis();   
+  }
+  
+
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+}
+
